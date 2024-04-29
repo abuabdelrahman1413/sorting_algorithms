d85e2266988688cea72b63ba392e10eb4dee9632
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-04-29 17:26:37 +0200

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, k;
+	int temp;
 
 	i = 0;
 
@@ -23,8 +24,12 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[k] > array[k + 1])
 			{
-				swap(&array[k], &array[k + 1]);
+				/*swap(&array[k], &array[k + 1]);*/
+				temp = array[k];
+				array[k] = array[k+1];
+				array[k+1] = temp;
 				print_array(array, size);
+
 			}
 		}
 		i++;
